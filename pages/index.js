fe1e09cf4698db628c4e646335da44ec7b34402b
Axel <axosar@gmail.com>
2021-03-26 11:56:21 -0600

    Test

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1>git push -u origin main
       </main>
 
       <footer className={styles.footer}>
