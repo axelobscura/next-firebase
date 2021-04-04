e5a10e583b97acee881e9042ca4f870d846c37a3
Axel <axosar@gmail.com>
2021-04-04 16:17:03 -0500

    agregando bootstrap

--- components/Contenedor.js
+++ components/Contenedor.js
@@ -5,7 +5,7 @@ function Contenedor({children}){
   return(
     <div className={styles.container}>
       <Head>
-        <title>FIREBASE APP TEST</title>
+        <title>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,7 +14,8 @@ function Contenedor({children}){
       </main>
 
       <footer className={styles.footer}>
-        test
+        <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
+        <p><small>Av. Insurgentes Sur # 1846 Col. Florida, C.P. 01030, México, CDMX.</small></p>
       </footer>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Contenedor from '../components/Contenedor';
 export default function Home() {
   return (
     <Contenedor>
-      <main>
+      <main className="entrada">
         <h1 className="title">PREGUNTAS Y RESPUESTAS</h1>
       </main>
     </Contenedor>
