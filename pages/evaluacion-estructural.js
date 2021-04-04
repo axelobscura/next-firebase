86c346fe776f8d79f1a858b313a21f06c6e83058
Axel <axosar@gmail.com>
2021-04-04 17:22:33 -0500

    formulario

---
+++ pages/evaluacion-estructural.js
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import Contenedor from '../components/Contenedor';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+
+export default function EvaluacionEstrutural() {
+  return (
+    <Contenedor>
+      <main className="entrada">
+        <h1 className="title">FORMATO DE CAPTURA DE DATOS PARA EVALUACIÓN ESTRUCTURAL</h1>
+        <hr />
+        <Form className="datos">
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Fecha: </Form.Label>
+              <Form.Control type="date" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Hora: </Form.Label>
+              <Form.Control type="time" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Duración visita: </Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Clave: </Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridAddress1">
+              <Form.Label>Nombre del evaluador: </Form.Label>
+              <Form.Control placeholder="Ingrese el nombre completo" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Control as="select" defaultValue="Choose...">
+                <option>Ingeniero o Arquitecto</option>
+                <option>Estudiante Ing/Arq.</option>
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+
+          <h2>Información general del inmueble:</h2>
+          <hr/>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>Nombre del inmueble: </Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+          
+          <Button variant="dark" type="submit" className="float-right">
+            ENVIAR DATOS
+          </Button>
+        </Form>
+      </main>
+    </Contenedor>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import Contenedor from '../components/Contenedor';
+import Button from 'react-bootstrap/Button'
 
 export default function Home() {
   return (
     <Contenedor>
       <main className="entrada">
-        <h1 className="title">PREGUNTAS Y RESPUESTAS</h1>
+        <h1 className="title">EVALUACIÓN ESTRUCTURAL</h1>
+        <Link href="evaluacion-estructural">
+          <Button variant="dark">REALIZAR EVALUACIÓN</Button>
+        </Link>
       </main>
     </Contenedor>
   )
