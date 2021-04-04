f42a521b378e44e91ce5041fbdaaab914977a4ff
Axel <axosar@gmail.com>
2021-04-04 15:36:25 -0500

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-//import Layout from '../components/Layout';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
+    <Layout>
       <main>
         <h1 className="title">PREGUNTAS Y RESPUESTAS</h1>
       </main>
+    </Layout>
   )
 }
