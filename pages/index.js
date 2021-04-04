109881de591f4de53716e47f9b558ec94f0a2747
Axel <axosar@gmail.com>
2021-04-04 15:09:05 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Layout>
       <main>
-        <h1 className="title">Firebase Client-Side</h1>
+        <h1 className="title">FIREBASE</h1>
       </main>
     </Layout>
   )
