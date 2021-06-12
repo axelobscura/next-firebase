5b81331f90f8c0e8db9f8c397efd8946379a164b
Axel Obscura <axosar@gmail.com>
2021-06-12 10:29:57 -0500

    Tot
    Tot

--- pages/resultados.js
+++ pages/resultados.js
@@ -11,7 +11,7 @@ export default function Resultados() {
         <div align="center">
           <img src="/logo_imcyc.png" alt="instituto mexicano del cemento y del concreto - evaluación estructural" title="instituto mexicano del cemento y del concreto - evaluación estructural" style={{ width: '190px', marginBottom: '20px' }} />
         </div>
-        <h1 className="title">GRACIAS!!!</h1>
+        <h1 className="title">GRACIAS TOTALES!!</h1>
         <h3>Sus datos han sido enviados con éxito...</h3>
         <h5>En breve uno de nuestros representantes se pondrá en contacto con usted...</h5>
       </main>
